17083de77c925f4e387e2c54912ad3d5f847546f
pamansaf <safwan.ssy@gmail.com>
2025-03-19 14:21:17 +0700

    Image Picker - from doc.expo

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LocalAPI from "./src/JSONServer/LocalAPI";
+import ImagePickerExample from "./src/ImagePicker";
 
 export default function App() {
   return (
     <View className="flex-1">
       <LocalAPI />
+      <ImagePickerExample />
     </View>
   );
 }
